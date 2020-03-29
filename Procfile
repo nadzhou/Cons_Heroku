eea60ee5b90b3d1bd01f260ad77695f526f4e4bb
nadzhou <ahmaddnavid@gmail.com>
2020-03-29 17:00:35 +0500

    some changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app_alpha.server
+web: gunicorn app_alpha:server
--- app_alpha.py
+++ app_alpha.py
@@ -16,8 +16,7 @@ import numpy as np
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-ser=app.server
-
+server = app.server
 app.layout = (
         html.Div([
             html.H1('MotvizPy Mini - Conservation Score Calculator', 
